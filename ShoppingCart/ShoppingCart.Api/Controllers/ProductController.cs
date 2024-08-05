da43928822d5abbfd6d71fb9bc3367bfbda7c705
Augusto Campos <augustosouzacampos99@gmail.com>
2024-08-05 13:03:54 -0300

    Implementations

---
+++ ShoppingCart/ShoppingCart.Api/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.Api.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
